55e504a055c1cd7fc353af94179835892a734e83
Carlos <carloskikea@gmail.com>
2016-11-16 23:44:08 -0500

    Add service for nearby fuelstation

--- app/models/fuelstation.rb
+++ app/models/fuelstation.rb
@@ -16,12 +16,18 @@ class Fuelstation < ActiveRecord::Base
 
   def self.getInfoFuelstations
     Fuelstation.find_by_sql("SELECT fst.id AS id, fst.name AS name, fst.description AS description, fst.image_url AS image_url,
-                                          COALESCE((select sum(point)/count(id) from comments where fuelstation_id = fst.id ),0) AS calification 
-                                          FROM fuelstations AS fst;")
+                            COALESCE((select sum(point)/count(id) 
+                            from comments where fuelstation_id = fst.id ),0) AS calification 
+                            FROM fuelstations AS fst;")
   end
 
+  #Busca todas las posiciones cercanas a la cordeada latitude/longitude ordenadas desendentemete por distancia, donde solo se mostraran las primeras 10
   def self.searchPosition(latitude, longitude)
-    Fuelstation.find_by_sql("")
+    Fuelstation.find_by_sql(["SELECT fst.id AS id, fst.name AS name, fst.description AS description, fst.image_url AS image_url,
+                                    COALESCE((select sum(point)/count(id) from comments where fuelstation_id = fst.id ),0) AS calification,
+                                    (6371 * acos(cos(radians(:lat)) * cos(radians(fst.latitude)) * cos(radians(fst.longitude) - radians(:long)) + sin(radians(:lat)) * sin(radians(fst.latitude )))) AS distance 
+                              FROM fuelstations AS fst
+                              ORDER BY distance LIMIT 10;", {:lat => latitude, :long => longitude}])
   end
 
 end
